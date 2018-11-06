058ab64199ed3eb5e6a89147f9b3ca09f3a62300
jackson.gunther <jackson.gunther@insidesales.com>
2018-11-06 12:01:32 -0700

    got the click working

--- tests/google.js
+++ tests/google.js
@@ -7,9 +7,11 @@ module.exports = {
         .assert.title('Google') // Make sure Site title matches
         .assert.visible('input[type=text]')
         .setValue('input[type=text]', 'nightwatch') // send values
-        .pause(10000)
-        .click('button[name=btnK]') // click on search box
         .pause(1000)
+        .useXpath().click("//*[@id='tsf']/div[2]/div/div[2]/div[2]/div/center/input[1]") // click on search box
+        .pause(1000)
+        // .url('http://www.facebook.com')
+        // .assert.title('Facebook - Log In or Sign Up')
         .end();
       }
 };
